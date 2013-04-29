c02970451f0907d6525159b7da15a48a76af6037
e487772 <rogerio@10.120.45.137>
2013-04-29 19:28:30 -0300

    segundo envio

---
+++ src/br/com/centralmusic/bean/SmithBean.java
@@ -0,0 +1,27 @@
+package br.com.centralmusic.bean;
+
+public class SmithBean {
+
+	private String serialNumber;
+	private String modelo;
+
+	
+	
+	
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
---
+++ src/br/com/centralmusic/controller/SmithController.java
@@ -0,0 +1,27 @@
+package br.com.centralmusic.controller;
+
+public class SmithController {
+
+	private String dataTeste = "2013/04/29";
+	
+	
+	
+	/** CONSTRUTOR */
+	public SmithController(){
+		
+		
+		
+		
+	}
+
+
+	public String getDataTeste() {
+		return dataTeste;
+	}
+
+	public void setDataTeste(String dataTeste) {
+		this.dataTeste = dataTeste;
+	}
+	
+	
+}
